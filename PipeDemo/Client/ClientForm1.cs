8d0ee6b7271340aad99b26260d7500b0be06b42e
GANWEIHUN <751646498@qq.com>
2020-01-12 11:18:23 +0800

    hello

---
+++ PipeDemo/Client/ClientForm1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class ClientForm1 : Form
+    {
+        private readonly RichTextBox _richTextBox;
+
+        public ClientForm1()
+        {
+            InitializeComponent();
+            Button button = new Button
+            {
+                Location = new Point(10, 20),
+                Size = new Size(100, 24),
+                Text = @"连接服务"
+            };
+            button.MouseClick += ButtonOnMouseClick;
+            Controls.Add(button);
+
+            _richTextBox = new RichTextBox
+            {
+                Location = new Point(button.Bounds.Left, button.Bottom + 10),
+                Size = new Size(300, 200)
+            };
+            Controls.Add(_richTextBox);
+            StartPosition = FormStartPosition.CenterScreen;
+            Size = new Size(400, 300);
+        }
+
+        private void ButtonOnMouseClick(object sender, MouseEventArgs mouseEventArgs)
+        {
+            _richTextBox.AppendText("开始连接服务 " + DateTime.Now);
+            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "myPipe", PipeDirection.InOut))
+            {
+                _richTextBox.AppendText("\n开始连接 myPipe " + DateTime.Now);
+                pipeClient.Connect();
+                Thread.Sleep(2000);
+                try
+                {
+                    using (StreamReader streamReader = new StreamReader(pipeClient))
+                    {
+                        _richTextBox.AppendText("\n开始读取服务端信息 " + DateTime.Now);
+                        while (true)
+                        {
+                            string text = streamReader.ReadLine();
+                            _richTextBox.AppendText("\n" + text);
+                            if (string.IsNullOrEmpty(text))
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ PipeDemo/Service/ServiceForm1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Windows.Forms;
+
+namespace Service
+{
+    public partial class ServiceForm1 : Form
+    {
+        private readonly RichTextBox _richTextBox;
+
+        public ServiceForm1()
+        {
+            InitializeComponent();
+            Button button = new Button
+            {
+                Location = new Point(10, 20),
+                Size = new Size(100, 24),
+                Text = @"启动服务"
+            };
+            button.MouseClick += ButtonOnMouseClick;
+            Controls.Add(button);
+
+            _richTextBox = new RichTextBox
+            {
+                Location = new Point(button.Bounds.Left, button.Bottom + 10),
+                Size = new Size(300, 200)
+            };
+            Controls.Add(_richTextBox);
+            _richTextBox.AppendText("我是服务端");
+            StartPosition = FormStartPosition.CenterScreen;
+            Size = new Size(400, 300);
+        }
+
+        private void ButtonOnMouseClick(object sender, MouseEventArgs e)
+        {
+            _richTextBox.AppendText("\n启动管道服务 myPipe " + DateTime.Now);
+            using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("myPipe", PipeDirection.InOut))
+            {
+                pipeServer.WaitForConnection();
+                _richTextBox.AppendText("\n客户端连接成功 " + DateTime.Now);
+                Trace.WriteLine("客户端连接成功");
+                //Thread.Sleep(1000);
+                try
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(pipeServer))
+                    {
+                        Trace.WriteLine("CanWrite" + pipeServer.CanWrite);
+                        streamWriter.AutoFlush = true;
+                        Trace.WriteLine("开始发送消息 ");
+                        streamWriter.Write("我是服务端 myPipe");
+                        streamWriter.WriteLine();
+                        streamWriter.Write(Process.GetCurrentProcess());
+                        streamWriter.WriteLine();
+                        streamWriter.Write("最后一行");
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    throw;
+                }
+            }
+        }
+    }
+}

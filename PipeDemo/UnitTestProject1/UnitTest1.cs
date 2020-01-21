0cd4652fd80863cd2329c445ccbd7a6855b195b0
ganweihun <751646498@qq.com>
2020-01-21 17:26:01 +0800

    Signed-off-by: ganweihun <751646498@qq.com>

---
+++ PipeDemo/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo {FileName = string.Format("{0}{1}", Environment.CurrentDirectory, @"\Service.exe")};
+            Process.Start(startInfo);
+            startInfo.FileName = string.Format("{0}{1}", Environment.CurrentDirectory, @"\Client.exe");
+            Process.Start(startInfo);
+        }
+    }
+}
